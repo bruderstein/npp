1e3a1e988e2e99369ba84f7559e651b3add1def3
Dave Brotherstone <davegb@pobox.com>
2012-04-30 21:40:51 +0200

    Version 5.9.8 with UDL2b9

--- PowerEditor/src/resource.h
+++ PowerEditor/src/resource.h
@@ -18,7 +18,7 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v5.9.8 with UDL2")
+#define NOTEPAD_PLUS_VERSION TEXT("Notepad++ v5.9.8 with UDL2b9")
 
 // should be X.Y : ie. if VERSION_DIGITALVALUE == 4, 7, 1, 0 , then X = 4, Y = 71 
 // ex : #define VERSION_VALUE TEXT("5.63\0")
